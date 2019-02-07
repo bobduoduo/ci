c6421f86d4103efa09b584f167197290a82ba4c9
xietiangui <2559602678@qq.com>
2019-02-07 20:06:57 +0800

    修改/github/ci/core/init.php

--- core/init.php
+++ core/init.php
@@ -66,6 +66,7 @@ util::path_exists(PATH_DATA."/cache");
 util::path_exists(PATH_DATA."/status");
 
 function autoload($classname) {
+<html>
 content:"E656"
     set_include_path(PATH_ROOT.'/library/');
     spl_autoload($classname); //replaces include/require
