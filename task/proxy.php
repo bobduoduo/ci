07da99b103e94041cca0e5733f9e92959c381934
xietiangui <2559602678@qq.com>
2019-02-22 09:01:02 +0800

    修改/github/ci/task/proxy.php

--- task/proxy.php
+++ task/proxy.php
@@ -2,6 +2,7 @@
 function doCurlGetRequest($url,$timeout = 5){
 
     $con = curl_init($url);
+}
     curl_setopt($con, CURLOPT_HEADER, false);
     curl_setopt($con, CURLOPT_RETURNTRANSFER,true);
     curl_setopt($con, CURLOPT_TIMEOUT, (int)$timeout);
