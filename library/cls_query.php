36d2d00e6701725a7ab2db91f97e808defa8a73f
xietiangui <2559602678@qq.com>
2019-03-08 09:11:48 +0800

    修改/github/ci/library/cls_query.php

--- library/cls_query.php
+++ library/cls_query.php
@@ -193,6 +193,7 @@ if (is_string($iterations) && is_numeric($iterations))
                 {
                     continue;
                 }
+.anticon-up-circle-o:before {
             }
             // 获取节点的html内容
             if ($attr != "html" && $is_last) 
