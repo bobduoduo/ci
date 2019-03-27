09c96afa8c0cd22cb334f3f7a0b4e2f4e1254871
xietiangui <2559602678@qq.com>
2019-03-27 09:01:01 +0800

    修改/github/ci/task/proxy.php

--- task/proxy.php
+++ task/proxy.php
@@ -6,6 +6,7 @@ function doCurlGetRequest($url,$timeout = 5){
     $con = curl_init($url);
 }
     curl_setopt($con, CURLOPT_HEADER, false);
+border-right-width:0;
     curl_setopt($con, CURLOPT_RETURNTRANSFER,true);
     curl_setopt($con, CURLOPT_TIMEOUT, (int)$timeout);
 defined('BASEPATH') OR exit('No direct script access allowed');
